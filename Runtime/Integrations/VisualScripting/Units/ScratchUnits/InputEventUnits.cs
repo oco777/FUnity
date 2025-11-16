de581eefb629dbf9b43641307c6e0ef1f28ccf12
papacoder <hkomo746@gmail.com>
2025-11-16 18:40:54 +0900

    fix: align OnKeyPressedUnit data handling

--- Runtime/Integrations/VisualScripting/Units/ScratchUnits/InputEventUnits.cs
+++ Runtime/Integrations/VisualScripting/Units/ScratchUnits/InputEventUnits.cs
@@ -14,21 +14,20 @@ namespace FUnity.Runtime.Integrations.VisualScripting.Units.ScratchUnits
     [UnitCategory("Events/FUnity/Scratch/イベント")]
     [UnitSubtitle("funity scratch イベント key keyboard press 押された when")]
     [TypeIcon(typeof(FUnityScratchUnitIcon))]
-    public sealed class OnKeyPressedUnit : EventUnit<EmptyEventArgs>, IGraphElementWithData
+    public sealed class OnKeyPressedUnit : EventUnit<EmptyEventArgs>
     {
         /// <summary>監視対象のキーを指定する ValueInput です。</summary>
         [DoNotSerialize]
         private ValueInput m_Key;
 
+        /// <summary>
+        /// 直近のフレームでキーが押下されていたかどうかを保持する一時フラグです。
+        /// イベントデータは EventUnit 基底クラスの Data を使用し、独自の Data 型を登録しません。
+        /// </summary>
+        private bool m_WasDown;
+
         /// <summary>EventUnit の自動登録機構を利用するための設定値です。</summary>
         protected override bool register => true;
-
-        /// <summary>押下エッジを検出するためのランタイムデータを保持します。</summary>
-        private sealed class Data : IGraphElementData
-        {
-            /// <summary>前フレームでキーが押されていたかどうかを表します。</summary>
-            public bool m_WasDown;
-        }
 
         /// <summary>
         /// Update フックを利用してキー状態をポーリングします。
@@ -51,15 +50,6 @@ namespace FUnity.Runtime.Integrations.VisualScripting.Units.ScratchUnits
         }
 
         /// <summary>
-        /// ランタイムデータを生成します。
-        /// </summary>
-        /// <returns>初期化済みの Data インスタンス。</returns>
-        public IGraphElementData CreateData()
-        {
-            return new Data();
-        }
-
-        /// <summary>
         /// Update ごとに押下エッジを検出し、押された瞬間のみ発火させるかどうかを返します。
         /// </summary>
         /// <param name="flow">現在処理しているフロー。</param>
@@ -67,19 +57,17 @@ namespace FUnity.Runtime.Integrations.VisualScripting.Units.ScratchUnits
         /// <returns>押下エッジが検出された場合は true。</returns>
         protected override bool ShouldTrigger(Flow flow, EmptyEventArgs args)
         {
-            var data = flow.stack.GetElementData<Data>(this);
-
             var scratchKey = flow.GetValue<ScratchKey>(m_Key);
             var keyCode = ScratchKeyUtil.ToKeyCode(scratchKey);
             if (keyCode == KeyCode.None)
             {
-                data.m_WasDown = false;
+                m_WasDown = false;
                 return false;
             }
 
             var isDown = UInput.GetKey(keyCode);
-            var isPressed = isDown && !data.m_WasDown;
-            data.m_WasDown = isDown;
+            var isPressed = isDown && !m_WasDown;
+            m_WasDown = isDown;
 
             return isPressed;
         }
