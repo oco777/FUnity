edb198f33f65ad97efcba4b07f8d6b4b95ca924a
papacoder <hkomo746@gmail.com>
2025-11-17 23:14:24 +0900

    fix: defer flow disposal in scratch event units

--- Runtime/Integrations/VisualScripting/Units/ScratchUnits/GreenFlagUnits.cs
+++ Runtime/Integrations/VisualScripting/Units/ScratchUnits/GreenFlagUnits.cs
@@ -132,33 +132,28 @@ namespace FUnity.Runtime.Integrations.VisualScripting.Units.ScratchUnits
         /// <param name="args">空のイベント引数。</param>
         private void TriggerWithThreadRegistration(GraphReference reference, EmptyEventArgs args)
         {
-            using (var flow = Flow.New(reference))
-            {
-                var routine = RunEventCoroutine(flow, args);
-                ScratchUnitUtil.StartScratchCoroutine(flow, routine);
-            }
+            var flow = Flow.New(reference);
+            var routine = RunEventCoroutine(flow, args);
+            ScratchUnitUtil.StartScratchCoroutine(flow, routine);
         }
 
         /// <summary>
-        /// EventUnit 標準のフロー実行をコルーチンでラップし、終了時に Flow を破棄します。
+        /// EventUnit 標準のフロー実行をコルーチンでラップし、Flow のライフサイクルは Visual Scripting 側に委ねます。
         /// </summary>
         /// <param name="flow">現在のフロー。</param>
         /// <param name="args">イベント引数。</param>
         /// <returns>実行完了までの列挙子。</returns>
         private IEnumerator RunEventCoroutine(Flow flow, EmptyEventArgs args)
         {
-            using (flow)
+            if (!ShouldTrigger(flow, args))
             {
-                if (!ShouldTrigger(flow, args))
-                {
-                    yield break;
-                }
-
-                AssignArguments(flow, args);
-
-                flow.StartCoroutine(trigger);
                 yield break;
             }
+
+            AssignArguments(flow, args);
+
+            flow.StartCoroutine(trigger);
+            yield break;
         }
     }
 }
